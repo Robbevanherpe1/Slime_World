b7e8f20b867ef671ab8fd828acab3a78e1e68e8c
Robbe <robbe.van.herpe@outlook.com>
2024-03-13 23:58:25 +0100

    fix : level1

--- LV1/atlas2Tileset.tsx
+++ LV1/atlas2Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="atlas2Tileset" tilewidth="16" tileheight="16" tilecount="1536" columns="48">
- <image source="../../Resources/Images_Map/atlas2.png" width="768" height="512"/>
+ <image source="atlas2.png" width="768" height="512"/>
 </tileset>
